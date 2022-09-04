ec4bcea0bb9250c81d2405148c91d2c67cb4cff6
Kingsley1618 <89419401+Kingsley1618@users.noreply.github.com>
2022-09-04 13:53:43 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Routing from "./Routes/Route";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default function App() {
+  return (
+    <div className="App">
+      <Routing />
+    </div>
+  );
+}
